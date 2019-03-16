56a3fde4b9036e4dc95b350eddbafc17c0552f13
Dan B <Dan-B1@users.noreply.github.com>
2019-03-16 12:23:09 +0000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+
+$config = array();
+
+//Teamspeak IP Address
+$config['ip'] = 'FILL ME IN'; 
+//Teamspeak Query Port (1101 Default)
+$config['queryport'] = '10011';
+//Teamspeak Server Port (9987 Default)
+$config['serverport'] = '9987';    
+//Teamspeak Query Username
+$config['username'] = 'FILL ME IN';
+//Teamspeak Query Password
+$config['password'] = 'FILL ME IN';            
+//Teamspeak Bot Username
+$config['botname'] = 'DB-Verify';
+//Create Channel (TRUE=YES, FALSE=NO)
+$config['channel'] = TRUE;
+//Name of verify file
+$config['vfile'] = "verify_new.php";
+//Site Name
+$config['sitename'] = "danb";
+
+
+//Verified Server Group
+$config['vservergroup'] = 16;
+//Verified Channel Group
+$config['vchannelgroup'] = 5;
+
+?>
